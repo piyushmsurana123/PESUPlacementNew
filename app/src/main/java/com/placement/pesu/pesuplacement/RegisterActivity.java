e240adcae058ce10ee8eabe6975deb55b2ae02c0
piyushmsurana123 <piyushmsurana123@gmail.com>
2018-11-19 21:53:39 +0530

    Working Register and Applying Logic

---
+++ app/src/main/java/com/placement/pesu/pesuplacement/RegisterActivity.java
@@ -0,0 +1,13 @@
+package com.placement.pesu.pesuplacement;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
